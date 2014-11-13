30a47d735237862172694f3158ee3d360202a518
Sauron <sauron@sauron-Q310.(none)>
2014-11-13 14:01:45 +0100

    Escenario coger lista pelis completado

--- ipm3_web/www/cgi-bin/model.py
+++ ipm3_web/www/cgi-bin/model.py
@@ -62,9 +62,9 @@ class Model:
         ### Movie data-related methods ###
 
     # Get new movie page
-    def movie_page_request(self, page):
+    def movie_page_request(self, params):
         # We assume Javascript already give us the page the browser wants
-        url = self.server_url + '/movies/page/' + str(page)
+        url = self.server_url + '/movies/page/' + str(params["page"])
         response = self.send_request('GET', url, None, None)
         return response.text
 
@@ -133,6 +133,10 @@ class Model:
         # Login
         if form.has_key("username") and form.has_key("passwd"):
             params = dict(username=form["username"].value, passwd=form["passwd"].value)
+        # Movie page request
+        if form.has_key("page"):
+        	params = dict(page = form["page"].value)
+		# Post comment
         return action, params
 
     # Creates a new cookie from a string
@@ -152,7 +156,8 @@ class Model:
         if action:
             ##Actions that do not need a cookie##
             if action == "movie_list":
-                pass
+                response = self.movie_page_request(params)
+                return response, None
             elif action == "movie_data":
                 pass
             elif action == "get_comments":
